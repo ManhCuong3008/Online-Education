680bcf136900c57337db73364540aa551796d754
ManhCuong3008 <62794412+ManhCuong3008@users.noreply.github.com>
2021-07-05 14:17:56 +0700

    First vision
    Add your code here

---
+++ OnlineEducation/OnlineEducation/Model/CourseDAO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OnlineEducation.Paging;
+using OnlineEduDB;
+
+namespace OnlineEducation.Model
+{
+    public class CourseDAO
+    {
+        MyDB myDB = new MyDB();
+       public List<Course>  getListCourseByName(string courseName)
+        {
+            return myDB.Courses.Where(c=> c.CourseName.Contains(courseName)).ToList();
+        }
+
+        public List<Course> getListByOffset( PageRequest page, string courseName)
+        {
+            return myDB.Courses.Where(c => c.CourseName.Contains(courseName)).OrderBy(c => c.CourseName).Skip(page.getOffset()).Take(page.getLimit()).ToList();
+        }
+    }
+}
